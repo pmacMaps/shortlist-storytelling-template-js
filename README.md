c9bc747198a58697cc1dd2451c04e18382d22462
Patrick McKinney <pnmcartography@gmail.com>
2018-08-30 08:26:36 -0400

    update legend creation method. upgrade app to version 2.7.0

--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 
 This fork of the [Esri Shortlist Story Map](https://github.com/Esri/shortlist-storytelling-template-js) provides the code to various customizations I have made in live deployments of the application.  
 
-The **Master Branch** contains the customizations for version **_2.2.3_**, while the [**ver-1.4.9 Branch**](https://github.com/pmacMaps/shortlist-storytelling-template-js/tree/ver-1.4.9) contains the customizations for version **_1.4.9_** of the Shortlist StoryMap.  The application underwent a major re-write at Version _2.x_.
+The **Master Branch** contains the customizations for version **_2.7.0_**, while the [**ver-1.4.9 Branch**](https://github.com/pmacMaps/shortlist-storytelling-template-js/tree/ver-1.4.9) contains the customizations for version **_1.4.9_** of the Shortlist StoryMap.  The application underwent a major re-write at Version _2.x_.
 
 Below is a listing of the customizations I have made:
 
-- A toggable legend located on the map 
-- Text placeholder in the header area of the app for the date the data was last updated
-- A map/app loading message for desktop and mobile versions of the app (v 1.4.9)
-
-**Note:** I have encountered instances where the 1.4.9 version of this app will load the tour points on desktop (above breakpoint), but not on mobile (below breakpoint).  I have not yet encountered this issue with version 2.2.3.
+- A toggable legend located on the map.  This uses the **esri request** module from the JS API.  It currently assumes map services as the source for legend elements.  
+- Text placeholder in the header area of the app for the date the data was last updated.
+- A map/app loading message for desktop and mobile versions of the app (v 1.4.9).
 
 Detailed descriptions of the customizations will be added to the [**Wiki**](https://github.com/pmacMaps/shortlist-storytelling-template-js/wiki) page in the near future.
--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -2,12 +2,130 @@ define(["dojo/topic"], function(topic) {
 	/*
 	* Custom Javascript to be executed while the application is initializing goes here
 	*/
+    // use esri request module to fetch map service legend info
+    // https://developers.arcgis.com/javascript/3/jsapi/esri.request-amd.html
+    require(["esri/request"], function(esriRequest) {
+            // function to get legend info in json format
+            function requestLegendJson(url) {
+                // request object
+                const requestHandle = esriRequest({
+                    url: url,
+                    content: {f: "json"},
+                    handleAs: 'json',
+                    callbackParamName: 'callback'
+                });           
+                // make request
+                requestHandle.then(
+                    function(response) {
+                        // legend container element
+                        const legendElement = $('#customLegend');                     
+                        // layers in response                        
+                        const layers = response.layers;
+                        // loop through each item in layers                    
+                        for (let item of layers) {                  
+                              // default layer name
+                              let layerName = item.layerName;
+                              // loop through legend properties
+                              for (let element of item.legend) {
+                                  // open legend list element
+                                  let legendContent = "<ul>";                      
+                                  // if the label element is empty, we'll use item.layerName as the name of the legend element
+                                  // if it is not, we'll use the label property of the legeng as the name for the legend element
+                                  if (element.label === " " || element.label === "") {
+                                    legendContent += `<li>${layerName}</li>`;
+                                 } else {
+                                    legendContent += `<li>${element.label}</li>`;     
+                                 }
+                                  // create image element
+                                  legendContent += `<li><img height=${element.height} width=${element.width} src="data:${element.contentType};base64,${element.imageData}" alt="legend icon representing ${layerName}" /></li>`;                              // close list element
+                                  legendContent += "</ul>";
+                                  // append legend item to legend
+                                  legendElement.append(legendContent);              
+                              }  // end for element of item.legend                                             
+                      } // end for item of layers                           
+                    }, // end function(response)
+                    function(error) {
+                        console.warn(`Error creating legend element for url ${url}`);
+                        console.warn("Error: ", error.message);
+                        // feel free to update error reporting code
+                    });
+            } // end requestLegendJson(url)
+            
+            // Map Service Legend Urls
+            const someMapServiceLegendUrl = 'https://[some_domain]/arcgis/rest/services/[some_service]/MapServer/legend';
+            const householdMedianIncomeLegendUrl = 'http://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer/legend';
+            // fake url to see how error function is called
+            const fakeLegendUrl = 'http://server.arcgisonline.com/arcgis/rest/services/Demographics/Living Index/MapServer/legend';
+                        
+            // call functions to get legend JSON and construct legend elements       
+            requestLegendJson(someMapServiceLegendUrl);
+            requestLegendJson(householdMedianIncomeLegendUrl);  
+            requestLegendJson(fakeLegendUrl);
+        }); // end require(["esri/request"], function(esriRequest) {})
+        
+        // Viewport Width
+        const viewportWidth = $(window).width();
+        // Custom Legend Button
+        // create this element in the index.html underneath <div id="map>
+        const customLegendBtn = $('#customLegendBtn');       
+        
+        // Custom Legend - add display/hide functionality to Legend button
+        // create <div id="customLegend> element in the index.html underneath <div id="map>
+        customLegendBtn.click(function() {            
+            $('#customLegend').toggle();
+        });
 
 	// The application is ready
 	topic.subscribe("tpl-ready", function(){
 		/*
 		* Custom Javascript to be executed when the application is ready goes here
-		*/     
+		*/
+        // Last Data Update Text
+        const lastDataUpdateDate = '8-30-2018' // update text to last data update date
+        // Element that will contain the last data update text
+        const dataUpdateText = `<span id="dataUpdate">Data Last Updated: ${lastDataUpdateDate}</span>`;
+        // Mobile title page element
+        const ccpaMobileTitlePage = $('#mobileIntro');
+        // Header Element
+        const ccpaHeader = $('#header');
+        // Viewport Width
+        const viewportWidth = $(window).width();
+        // Custom Legend Button
+        const customLegendBtn = $('#customLegendBtn');
+        // element that contains addtional tabs
+        const moreTabsAnchorElement = $('a.dropdown-toggle');
+        
+        // add "More Tabs" text to tab element
+        // this is a personal preference. I think it is nice to let people
+        // know that this element contains additional tabs
+        moreTabsAnchorElement.append('<span>More Tabs</span>');  
+    
+        // Custom Legend - If screen width is above breakpoint, display legend button
+        if (viewportWidth > 768) {
+            customLegendBtn.show();
+        } else {
+            $('#mobileThemeList li.mobileTitleThemes').click(function() {
+                customLegendBtn.show(); // show legend button once map is visible below breakpoint
+            });
+        }
+        
+        // Append last data update text to appropriate element based upon viewport size
+        $(document).ready(function() {
+            // above breakpoint
+            if (viewportWidth > 768 ) {
+                ccpaHeader.append(dataUpdateText);
+            } else {
+                ccpaMobileTitlePage.prepend(dataUpdateText); // below breakpoint    
+            }
+        });
+
+        // place last data update text on header if screen size changes to above breakpoint    
+        $(window).resize(function() {
+            if (viewportWidth > 768) {
+                ccpaHeader.append(dataUpdateText);
+            }
+        });        
+        
 	});
 
 	// Tab navigation.  This is fired anytime the tab is selected/changed.
