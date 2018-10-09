ed39bf01b73261a38d5b07da58608985361fa626
Patrick McKinney <pnmcartography@gmail.com>
2018-10-09 11:58:08 -0400

    use es2015 code so it works in IE 11

--- shortlist-2x/app/custom-scripts.js
+++ shortlist-2x/app/custom-scripts.js
@@ -5,57 +5,83 @@ define(["dojo/topic"], function(topic) {
     // use esri request module to fetch map service legend info
     // https://developers.arcgis.com/javascript/3/jsapi/esri.request-amd.html
     require(["esri/request"], function(esriRequest) {
+            'use strict';
             // function to get legend info in json format
             function requestLegendJson(url) {
                 // request object
-                const requestHandle = esriRequest({
+                var requestHandle = esriRequest({
                     url: url,
-                    content: {f: "json"},
+                    content: { f: "json" },
                     handleAs: 'json',
                     callbackParamName: 'callback'
-                });           
+                });
                 // make request
-                requestHandle.then(
-                    function(response) {
-                        // legend container element
-                        const legendElement = $('#customLegend');                     
-                        // layers in response                        
-                        const layers = response.layers;
-                        // loop through each item in layers                    
-                        for (let item of layers) {                  
-                              // default layer name
-                              let layerName = item.layerName;
-                              // loop through legend properties
-                              for (let element of item.legend) {
-                                  // open legend list element
-                                  let legendContent = "<ul>";                      
-                                  // if the label element is empty, we'll use item.layerName as the name of the legend element
-                                  // if it is not, we'll use the label property of the legeng as the name for the legend element
-                                  if (element.label === " " || element.label === "") {
-                                    legendContent += `<li>${layerName}</li>`;
-                                 } else {
-                                    legendContent += `<li>${element.label}</li>`;     
-                                 }
-                                  // create image element
-                                  legendContent += `<li><img height=${element.height} width=${element.width} src="data:${element.contentType};base64,${element.imageData}" alt="legend icon representing ${layerName}" /></li>`;                              // close list element
-                                  legendContent += "</ul>";
-                                  // append legend item to legend
-                                  legendElement.append(legendContent);              
-                              }  // end for element of item.legend                                             
-                      } // end for item of layers                           
-                    }, // end function(response)
-                    function(error) {
-                        console.warn(`Error creating legend element for url ${url}`);
-                        console.warn("Error: ", error.message);
-                        // feel free to update error reporting code
-                    });              
-            } // end requestLegendJson(url)
+                requestHandle.then(function (response) {
+                    // legend container element
+                    var legendElement = $('#customLegend');
+                    // layers in response                        
+                    var layers = response.layers;
+                    // loop through each item in layers                    
+                    for (var _iterator = layers, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+                        var _ref;
+
+                        if (_isArray) {
+                            if (_i >= _iterator.length) break;
+                            _ref = _iterator[_i++];
+                        } else {
+                            _i = _iterator.next();
+                            if (_i.done) break;
+                            _ref = _i.value;
+                        }
+
+                        var item = _ref;
+
+                        // default layer name
+                        var layerName = item.layerName;
+                        // loop through legend properties
+                        for (var _iterator2 = item.legend, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+                            var _ref2;
+
+                            if (_isArray2) {
+                                if (_i2 >= _iterator2.length) break;
+                                _ref2 = _iterator2[_i2++];
+                            } else {
+                                _i2 = _iterator2.next();
+                                if (_i2.done) break;
+                                _ref2 = _i2.value;
+                            }
+
+                            var element = _ref2;
+
+                            // open legend list element
+                            var legendContent = "<ul>";
+                            // if the label element is empty, we'll use item.layerName as the name of the legend element
+                            // if it is not, we'll use the label property of the legeng as the name for the legend element
+                            if (element.label === " " || element.label === "") {
+                                legendContent += '<li>' + String(layerName) + '</li>';
+                            } else {
+                                legendContent += '<li>' + String(element.label) + '</li>';
+                            }
+                            // create image element
+                            legendContent += '<li><img height=' + String(element.height) + ' width=' + String(element.width) + ' src="data:' + String(element.contentType) + ';base64,' + String(element.imageData) + '" alt="legend icon representing ' + String(layerName) + '" /></li>'; // close list element
+                            legendContent += "</ul>";
+                            // append legend item to legend
+                            legendElement.append(legendContent);
+                        } // end for element of item.legend                                             
+                    } // end for item of layers                           
+                }, // end function(response)
+                function (error) {
+                    console.warn('Error creating legend element for url ' + String(url));
+                    console.warn("Error: ", error.message);
+                    // feel free to update error reporting code
+                });
+            } // end requestLegendJson(url)            
         
             // Map Service Legend Urls
-            const municipalBoundaryLegendUrl = 'https://gis.ccpa.net/arcgiswebadaptor/rest/services/ArcGIS_Online/MunicipalBoundaries/MapServer/legend';
-            const waterTrailsLegendUrl = 'https://gis.ccpa.net/arcgiswebadaptor/rest/services/ArcGIS_Online/WaterTrailsData/MapServer/legend';
+            var municipalBoundaryLegendUrl = 'https://gis.ccpa.net/arcgiswebadaptor/rest/services/ArcGIS_Online/MunicipalBoundaries/MapServer/legend';
+            var waterTrailsLegendUrl = 'https://gis.ccpa.net/arcgiswebadaptor/rest/services/ArcGIS_Online/WaterTrailsData/MapServer/legend';
             // fake url to see how error function is called
-            const fakeLegendUrl = 'http://server.arcgisonline.com/arcgis/rest/services/Demographics/Living Index/MapServer/legend';
+            var fakeLegendUrl = 'http://server.arcgisonline.com/arcgis/rest/services/Demographics/Living Index/MapServer/legend';
                         
             // call functions to get legend JSON and construct legend elements       
             requestLegendJson(municipalBoundaryLegendUrl);
@@ -63,7 +89,7 @@ define(["dojo/topic"], function(topic) {
             requestLegendJson(fakeLegendUrl);
         }); // end require(["esri/request"], function(esriRequest) {})
         
-    // Viewport Width
+        // Viewport Width
         const viewportWidth = $(window).width();
         // Custom Legend Button
         // create this element in the index.html underneath <div id="map>
